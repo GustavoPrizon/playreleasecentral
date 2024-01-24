4cf598dfc68257c272315474c3ba44878aed5fcf
GustavoPrizon <gustavoprizon@gmail.com>
2024-01-24 01:28:55 -0300

    feat: start create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
+import styles from "./Header.module.css";
+
+import Logo from "../assets/logo.png";
+
 export function Header() {
-  return <strong>PlayReleaseCentral</strong>;
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt="PlayReleaseCentral" />
+      <strong>PlayReleaseCentral</strong>
+    </header>
+  );
 }
